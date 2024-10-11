07ea7451afb52b062fec2660c209e4294df29907
Bobur Farhodov <farhodovbobur@MacBook-Pro-Bobur.local>
2024-10-11 20:14:41 +0500

    MINOR: migrations

---
+++ database/migrations/2024_10_11_150035_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('model')->nullable();
+            $table->string('color')->nullable();
+            $table->year('year')->nullable();
+            $table->string('plate_number')->nullable();
+            $table->foreignId('owner_id')->constrained('licenses')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
---
+++ database/migrations/2024_10_11_150136_create_fines_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fines', function (Blueprint $table) {
+            $table->id();
+            $table->string('plate_number')->nullable();
+            $table->float('amount')->nullable();
+            $table->enum('type', ['red_line', 'speed'])->nullable();
+            $table->foreignId('car_id')->constrained('cars')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fines');
+    }
+};
